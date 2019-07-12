149e334d6a775583bfb261cdaa87f3417d3a6eaf
Jehferson Mello <hayros666@gmail.com>
2019-07-12 18:25:35 +1200

    Revert "[OpenSuSE] Corrected scripts fort rocBlas and rocfft"
    This reverts commit 848a3c4296fc7218b18cc2a13d8a8df85f971316.

--- distro_install_scripts/OpenSUSE/common/component_scripts/03_02_rocfft.sh
+++ distro_install_scripts/OpenSUSE/common/component_scripts/03_02_rocfft.sh
@@ -65,7 +65,6 @@ if [ ${ROCM_FORCE_GET_CODE} = true ]; then
 fi
 
 cd ${SOURCE_DIR}/rocFFT
-sed -i '/find_package( CUDA QUIET )/d' CMakeLists.txt
 mkdir -p build/release
 cd build/release
 CXX=${ROCM_INPUT_DIR}/hcc/bin/hcc cmake -DCPACK_PACKAGING_INSTALL_PREFIX=${ROCM_OUTPUT_DIR}/ -DCPACK_GENERATOR=RPM ${ROCM_CPACK_RPM_PERMISSIONS} -DCMAKE_BUILD_TYPE=${ROCM_CMAKE_BUILD_TYPE} -DCMAKE_INSTALL_PREFIX=${ROCM_OUTPUT_DIR}/ ../../
